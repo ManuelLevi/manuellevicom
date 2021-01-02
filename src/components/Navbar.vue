f57e14932eff74e7c55bbbd70734555eeac468e2
manuellevi <mlevif@gmail.com>
2021-01-02 22:48:52 +0000

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <v-navigation-drawer v-model="drawer" dark app mini-variant mini-variant-width="100" class='cyan darken-4'>
+
+            <v-list>
+                <v-list-item class="mb-12">
+                    <v-list-item-action>
+                    </v-list-item-action>
+                </v-list-item>
+            </v-list>
+            <v-list flat>
+                <v-list-item router to='/'>
+                    <v-list-item-content>
+                        <v-list-item-subtitle align="center">Home</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+            <v-list style="position: absolute; bottom:0" class="ml-3" flat>
+                <v-list-item router to='/'>
+                    <v-list-item-action>
+                        <v-list-item-subtitle>Signout</v-list-item-subtitle>
+                    </v-list-item-action>
+                </v-list-item>
+
+            </v-list>
+        </v-navigation-drawer>
+
+    </nav>
+
+</template>
